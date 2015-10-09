32f82bbb65be023a12e0953edd2fbab78819d7ef
Omari Mayers-Walker <omari@fastmail.fm>
2015-10-09 09:12:13 -0400

    required modules, created url request

---
+++ index.js
@@ -0,0 +1,15 @@
+// requiring modules; url variable
+var request = require('request'),
+    cheerio = require('cheerio'),
+    url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 21202;
+
+// downloading url via request module function
+request(url, function(error, response, body) {
+    if (!error) {
+        var $ = cheerio.load(body),
+            temperature = $("[data-variable='temperature'] .wx-value").html();
+        console.log("It's " + temperature + " degrees Fahrenheit.");
+    } else {
+        console.log("We've encountered an error: " + error);
+    }
+});
